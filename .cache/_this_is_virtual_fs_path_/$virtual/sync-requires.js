7cd6ccd1b25fcfb2db5791727ea17b4b30ac596d
Theresia Purnomo <theresiap2022@gmail.com>
2021-07-19 13:20:09 +0800

    changes

--- .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/404.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/page-2.js")),
