17cbb4045efb28b530b0d3e5f2b6eda101c4efc5
Theresia Purnomo <theresiap2022@gmail.com>
2021-07-19 13:14:12 +0800

    init

---
+++ .cache/sync-requires.js
@@ -0,0 +1,13 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/page-2.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/purnot1/Desktop/BSD/theresia-gatsby-site/src/pages/using-typescript.tsx"))
+}
+
